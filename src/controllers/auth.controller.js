3d6f09b2bbaed9f2c31743cf20df44da1942e45b
Juan Pablo Corzo Barrera <juanpablocorzobarrera@gmail.com>
2024-03-26 14:40:12 -0300

    carga de users lista

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,19 @@
-export const register = (req, res) => {
+import User from "../schemas/user.model.js";
+
+export const register = async (req, res) => {
   const { email, password, username } = req.body;
-  res.send("registrando");
+
+  try {
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+    await newUser.save();
+    res.send("registrando");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const login = (req, res) => res.send("Login");
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import app from "./app.js";
 import initDB from "./db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(3000);
+app.listen(process.env.PORT);
 const connectToDB = initDB();
 
 connectToDB();
-console.log(`server on port ${3000}`);
+console.log(`server on port ${process.env.PORT}`);
