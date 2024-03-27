12a8e88f3f34135b7e0f90f5b2f9d5647fb5b400
Juan Corzo Barrera <juanpablocorzobarrera@gmail.com>
2024-03-27 13:25:04 -0300

    authcontroller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,18 @@
 import User from "../schemas/user.model.js";
+import bcrypt from 'bcryptjs'
+
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
+
+    const passwordHash = await bcrypt.hash(password,10)
+
     const newUser = new User({
       username,
       email,
-      password,
+      password:passwordHash,
     });
     await newUser.save();
     res.send("registrando");
