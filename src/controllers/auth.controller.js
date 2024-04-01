a5ee1ede6cd5538260620f899fdf1502beecae26
juan corzo barrera <juanpablocorzobarrera@gmail.com>
2024-04-01 20:23:25 -0300

    rdy login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,16 @@
 import User from "../schemas/user.model.js";
-import bcrypt from 'bcryptjs'
-
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
-
-    const passwordHash = await bcrypt.hash(password,10)
+    const passwordHash = await bcrypt.hash(password, 10);
 
     const newUser = new User({
       username,
       email,
-      password:passwordHash,
+      password: passwordHash,
     });
     await newUser.save();
     res.send("registrando");
@@ -21,4 +19,40 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send("Login");
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userFound = await User.findOne({ email });
+
+    if (!userFound)
+      return res.status(400).json({
+        message: ["The email does not exist"],
+      });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: ["The password is incorrect"],
+      });
+    }
+
+    const token = await createAccessToken({
+      id: userFound._id,
+      username: userFound.username,
+    });
+
+    res.cookie("token", token, {
+      httpOnly: process.env.NODE_ENV !== "development",
+      secure: true,
+      sameSite: "none",
+    });
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
