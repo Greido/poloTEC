63068b9df0a58a7aee4b86999d4588646ce105b6
juan corzo barrera <juanpablocorzobarrera@gmail.com>
2024-04-02 02:06:40 -0300

    running register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,28 @@
 import User from "../schemas/user.model.js";
-import bcrypt from 'bcryptjs'
-
+import bcrypt from "bcryptjs";
+//import { createAccessToken } from "../libs/jwt.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
-
-    const passwordHash = await bcrypt.hash(password,10)
+    const passwordHash = await bcrypt.hash(password, 10);
 
     const newUser = new User({
       username,
       email,
-      password:passwordHash,
+      password: passwordHash,
     });
-    await newUser.save();
+
+    const userSaved = await newUser.save();
+
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+    });
+
     res.send("registrando");
   } catch (error) {
     console.log(error);
