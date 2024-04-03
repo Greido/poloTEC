45ff0529091debb5cd20ebcb96f478bcfbd5ceab
juan corzo barrera <juanpablocorzobarrera@gmail.com>
2024-04-02 23:42:53 -0300

    login v1  completo

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,4 +30,29 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send("Login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ message: "Not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect password" });
+
+    const token = await createAccessToken({ id: userFound._id });
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      message: "Login successful",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
